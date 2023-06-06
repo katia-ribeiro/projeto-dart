ee5738bf323474dc23b4e5ad18fbf4bd9f3c83c9
katia <katiaferreiradesousaribeiro@gmail.com>
2023-06-06 19:06:59 -0300

    teste constante e booleanas

---
+++ meu-app-dart/lib/tipos/const.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  const String variavel = "ABC";
+  const int variavel1 = 1;
+  const bool variavel2 = true;
+  print(variavel);
+  print(variavel1);
+  print(variavel2);
+}
