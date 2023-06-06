c13efdd9cff0146f13e98854869a192b3fbf0da9
katia <katiaferreiradesousaribeiro@gmail.com>
2023-06-06 17:50:40 -0300

    criação da pasta tipos para variaveis

---
+++ meu-app-dart/bin/meu_app_dart.dart
@@ -0,0 +1,5 @@
+import 'package:meu_app_dart/meu_app_dart.dart' as meu_app_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${meu_app_dart.calculate()}!');
+}
---
+++ meu-app-dart/test/meu_app_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:meu_app_dart/meu_app_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
